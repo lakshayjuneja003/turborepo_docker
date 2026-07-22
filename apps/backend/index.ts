23daa62293781c01066ece38bf86a5ff4e61836f
lakshayjuneja <lakshayjuneja003@gmail.com>
2026-07-22 13:34:47 +0530

    push to dockerhub

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -26,7 +26,7 @@ app.get("/users", async (req, res) => {
     res.status(200).json({ users }); 
     return ;
 })
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
